b1ff44a0ef36a775166d25b57735c4c0c7b5c9ea
Hannah Waruguru <hannahwaruguru@Hannahs-MacBook-Pro.local>
2025-04-09 09:55:44 +0300

    add a beam

--- sketch.js
+++ sketch.js
@@ -18,10 +18,27 @@ function setup() {
     base_height: 0.45,
     num_lights: 20,
     brightnesses: [],
+    beam_on: false,
+    hover: function () {
+      //console.log('hover')
+      this.x += random(-2, 2);
+      this.y += random(-1, 1);
+    },
+    beam: function () {
+      fill(255, 255, 100, 150);
+      if (random() > 0.1) {
+        beginShape();
+        vertex(this.x - this.width * 0.25, this.y);
+        vertex(this.x + this.width * 0.25, this.y);
+        vertex(this.x + this.width * 0.35, height - 100);
+        vertex(this.x - this.width * 0.35, height - 100);
+        endShape(CLOSE);
+      }
+    },
   };
 
   for (var i = 0; i < flying_saucer.num_lights; i++) {
-    flying_saucer.brightnesses.push((i * 20)%255);
+    flying_saucer.brightnesses.push((i * 20) % 255);
   }
 }
 
@@ -31,14 +48,16 @@ function draw() {
   //draw the ground
   fill(0, 50, 0);
   rect(0, height - 100, width, 100);
-
+  if (flying_saucer.beam_on == true) {
+    flying_saucer.beam();
+  }
   //draw the flying saucer
   fill(175, 238, 238);
   arc(
     flying_saucer.x,
     flying_saucer.y,
     flying_saucer.width * flying_saucer.window_width,
-        flying_saucer.height * flying_saucer.window_height,
+    flying_saucer.height * flying_saucer.window_height,
     PI,
     TWO_PI
   );
@@ -47,7 +66,7 @@ function draw() {
     flying_saucer.x,
     flying_saucer.y,
     flying_saucer.width,
-    flying_saucer.height/2,
+    flying_saucer.height / 2,
     PI,
     TWO_PI
   );
@@ -60,29 +79,28 @@ function draw() {
     0,
     PI
   );
-  flying_saucer.x += random(-2, 2);
-  flying_saucer.y += random(-1, 1);
+  flying_saucer.hover();
+
   fill(255);
   var incr = flying_saucer.width / (flying_saucer.num_lights - 1);
   for (var i = 0; i < flying_saucer.num_lights; i++) {
-    var x = flying_saucer.x - flying_saucer.width/2 + i * incr;
+    var x = flying_saucer.x - flying_saucer.width / 2 + i * incr;
     fill(flying_saucer.brightnesses[i]);
-    ellipse(
-      x ,
-      flying_saucer.y,
-      5,
-      5
-    )
-      flying_saucer.brightnesses[i] += 5;
-      if(flying_saucer.brightnesses[i] > 255)
-      {
-          flying_saucer.brightnesses[i] = 100;
-      }
+    ellipse(x, flying_saucer.y, 5, 5);
+    flying_saucer.brightnesses[i] += 5;
+    if (flying_saucer.brightnesses[i] > 255) {
+      flying_saucer.brightnesses[i] = 100;
+    }
   }
-  
-  
-  flying_saucer.x += random(-1,1);
-  flying_saucer.y += random(-1,1);
-  
-  
+
+  flying_saucer.x += random(-1, 1);
+  flying_saucer.y += random(-1, 1);
+}
+
+function keyPressed() {
+  flying_saucer.beam_on = true;
+}
+
+function keyReleased() {
+  flying_saucer.beam_on = false;
 }
